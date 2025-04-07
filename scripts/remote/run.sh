61fbbd4fac06b14ed66c2851b5536390436aade3
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-04-07 20:13:21 +0200

    Deploying RbeesoftNginx

--- scripts/remote/run.sh
+++ scripts/remote/run.sh
@@ -5,4 +5,5 @@ export REACT_APP_API_BASE_URL="https://test.rbeesoft-apps.nl"
 
 SERVICE_NAME=${1}
 
+docker compose pull
 docker compose up -d ${SERVICE_NAME} && docker compose logs -f ${SERVICE_NAME}
